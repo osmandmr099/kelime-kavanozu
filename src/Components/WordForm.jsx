bd2f0213b0597e4ab4bb7074a541348ce91031f4
Osman <osman.dmr099@gmail.com>
2026-05-05 23:26:29 +0300

    Add files via upload

--- src/Components/WordForm.jsx
+++ src/Components/WordForm.jsx
@@ -18,10 +18,10 @@ function WordForm(props) {
     <div className="glass-card mb-5">
       <form onSubmit={butonaTiklandi} className="row g-3 align-items-center">
         <div className="col-md-5">
-          <input type="text" className="form-control form-control-lg glass-input" placeholder="Yeni Kelime (Örn: Serendipity)" value={k} onChange={(olay) => setK(olay.target.value)} />
+          <input type="text" className="form-control form-control-lg glass-input" placeholder="Yeni Kelime (Örn: Apple)" value={k} onChange={(olay) => setK(olay.target.value)} />
         </div>
         <div className="col-md-5">
-          <input type="text" className="form-control form-control-lg glass-input" placeholder="Anlamı (Örn: Mutlu tesadüf)" value={a} onChange={(olay) => setA(olay.target.value)} />
+          <input type="text" className="form-control form-control-lg glass-input" placeholder="Anlamı (Örn: Elma)" value={a} onChange={(olay) => setA(olay.target.value)} />
         </div>
         <div className="col-md-2 d-grid">
           <button type="submit" className="btn btn-light btn-lg fw-bold text-primary">Ekle ✨</button>
